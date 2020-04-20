f700a050f14a057c1921eee7c25b43647a8f711f
mvgmenezes <mvgmenezes@gmail.com>
2020-04-20 20:29:03 +0300

    Using list view children[]

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,48 +10,46 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
-      child: SingleChildScrollView(
-              child: Column(
-                  children: transactions.map((trans) {
-                    return Card(
-                        child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin:
-                              EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.purple, width: 2)),
-                          padding: EdgeInsets.all(10),
-                          child: Text('R\$ ${trans.amount}',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.purple)),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              trans.title,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16),
+      height: 300,
+      child: ListView(
+                children: transactions.map((trans) {
+                  return Card(
+                      child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: EdgeInsets.all(10),
+                        child: Text('R\$ ${trans.amount}',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple)),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            trans.title,
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                          ),
+                          Text(
+                            DateFormat.yMMMEd().format(trans.date),
+                            style: TextStyle(
+                              color: Colors.grey,
                             ),
-                            Text(
-                              DateFormat.yMMMEd().format(trans.date),
-                              style: TextStyle(
-                                color: Colors.grey,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ));
-                  }).toList(),
-                ),
-      ),
+                          )
+                        ],
+                      )
+                    ],
+                  ));
+                }).toList(),
+              ),
     );
   }
 }
