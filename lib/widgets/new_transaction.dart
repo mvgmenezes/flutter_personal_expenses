8dcbd68eb7517b2ddb59c921e1c0d3939332fdec
mvgmenezes <mvgmenezes@gmail.com>
2020-06-06 11:23:45 +0300

    Adding modal to add new transactions

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTx;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData(){
     final enteredTitle = titleController.text;
@@ -14,8 +21,13 @@ class NewTransaction extends StatelessWidget {
     if(enteredTitle.isEmpty || enteredAmount <= 0 ){
       return;
     }
-    addTx(titleController.text, double.parse(amountController.text));
+    widget.addTx(
+        titleController.text,
+        double.parse(amountController.text)
+    );
+    Navigator.of(context).pop();
   }
+
   @override
   Widget build(BuildContext context) {
     return Card(
