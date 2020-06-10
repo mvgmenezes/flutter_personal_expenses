89c9244fe417b6353e2d2441f45f1bcc5bffe476
Marcus Menezes <marcus.menezes@kuehne-nagel.com>
2020-06-10 09:15:00 +0300

    Adding the new transaction scrolled and get the size of keyboard

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -54,52 +54,59 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child:Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: _titleController,
-              onSubmitted: (_)=> _submitData(),
-              //onChanged: (value) { this.titleInput = value;},
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: _amountController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_)=> _submitData(),
-              //onChanged: (value) { this.amountInput = value;},
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: <Widget>[
-                Expanded(
-                  child: Text(_selectedDate == null?
-                        'No Date chosen!':
-                        'Picked Date: ${DateFormat.yMMMd().format(_selectedDate)}'),
-                ),
-                FlatButton(
-                  textColor: Theme.of(context).primaryColor,
-                  child: Text('Choose date', style: TextStyle(fontWeight: FontWeight.bold ),),
-                  onPressed: _presentDatePicker,
-                )
-              ],),
-            ),
-            RaisedButton(
-              child: Text('Add transaction'),
-              onPressed: _submitData,
-              textColor: Theme.of(context).textTheme.button.color,
-              color: Theme.of(context).primaryColor,
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child:Container(
+          padding: EdgeInsets.only(
+              top: 10,
+              left: 10,
+              right: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10 //bottom return size of keyboard
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: _titleController,
+                onSubmitted: (_)=> _submitData(),
+                //onChanged: (value) { this.titleInput = value;},
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: _amountController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_)=> _submitData(),
+                //onChanged: (value) { this.amountInput = value;},
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: <Widget>[
+                  Expanded(
+                    child: Text(_selectedDate == null?
+                          'No Date chosen!':
+                          'Picked Date: ${DateFormat.yMMMd().format(_selectedDate)}'),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text('Choose date', style: TextStyle(fontWeight: FontWeight.bold ),),
+                    onPressed: _presentDatePicker,
+                  )
+                ],),
+              ),
+              RaisedButton(
+                child: Text('Add transaction'),
+                onPressed: _submitData,
+                textColor: Theme.of(context).textTheme.button.color,
+                color: Theme.of(context).primaryColor,
 
-            )
-          ],
-        ),
-      )
+              )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
